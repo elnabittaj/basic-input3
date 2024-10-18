dc648c766f80e99181b4cdfaff04999f45d24f6a
elnabittaj <elnabittaj@gmail.com>
2024-10-18 15:57:52 +0530

    Add files via upload

---
+++ basicinput3.py
@@ -0,0 +1,9 @@
+number1=int(input("Enter first number"))
+number2=int(input("Enter second number"))
+#num_sum=number1+number2
+#num_div=number1/number2
+#print("sum= ", num_sum, "division= ",num_div)
+#print("is num1 greater than num2?:", number1>number2)
+#print("is num1 greater than or equal to num2?:",number1>=number2)
+print("logical AND:",number1>0 and number2>0)
+print("logical OR:",number2>number1)
